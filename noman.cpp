5bcd42c825a1d3d51323b485b37ba154d77357ff
Noman-Nabil <nomannabil30@gmail.com>
2017-03-25 13:08:07 +0600

    Number 4

---
+++ noman.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    float a,b,c,result;
+    cout<<"Enter the 1st number:";cin>>a;
+    cout<<"Enter the 2nd number:";cin>>b;
+    cout<<"Enter the 3rd number:";cin>>c;
+    result=a*(b-c)+1.37;
+    cout<<"The result is:"<<result<<endl;
+
+    return 0;
+
+
+}
+
